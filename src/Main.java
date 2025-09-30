52b9ec05fabef0f2eee02554afc2c8a4e89e930a
nolongeraregistereduser <mohamedzouhairi023@gmail.com>
2025-09-30 14:05:03 +0100

    adding jdbc jar library to my project connection succesful

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,18 @@
+import util.DBUtil;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import util.DBUtil;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
-
+        try (Connection conn = DBUtil.getConnection()) {
+            System.out.println("Connection successful!");
+        } catch (SQLException e) {
+            System.out.println("Connection failed: " + e.getMessage());
+        }
     }
 }
